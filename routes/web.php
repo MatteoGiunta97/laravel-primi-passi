47ebdd2d33909952cdb388abb74a0465c0399f63
MatteoGiunta97 <matteogiunta@outlook.it>
2024-05-18 18:48:53 +0200

    milestone 1

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'greeting' => 'Hello World'
+    ];
+
+    return view('home', $data);
 });
